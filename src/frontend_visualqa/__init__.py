4c5ab5202a91808785be679852af5d61ce122e1c
dhruvbatra <dhruvbatra@users.noreply.github.com>
2026-03-21 14:35:29 -0700

    Bump version to 0.3.5

--- src/frontend_visualqa/__init__.py
+++ src/frontend_visualqa/__init__.py
@@ -6,7 +6,7 @@ from importlib import import_module
 from typing import Any
 
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 _LAZY_EXPORTS = {
     "ActionExecutor": ("frontend_visualqa.actions", "ActionExecutor"),
